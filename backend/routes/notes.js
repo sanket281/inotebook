c8f702e278381628f21937f30463da3865713219
sanket281 <shingadiyasanko@gmail.com>
2023-04-10 21:56:41 +0530

    backend-notes complete

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -41,4 +41,49 @@ router.post("/addnote",fetchuser,
   }
 );
 
+// ROUTE 3: Update an existing Note using : PUT "api/auth/updatenote". Login required
+router.put("/updatenote/:id", fetchuser, async (req,res)=>{
+    const {title, description, tag} = req.body;
+    try {
+       //Create a newNote object
+       const newNote = {};
+       if(title){newNote.title = title};
+       if(description){newNote.description = description};
+       if(tag){newNote.tag = tag};
+       
+       //Find the note to be updated amd update it
+       let note = await Note.findById(req.params.id);
+       if(!note){return res.status(404).send("Not Found")} //Note is not there in the database
+       if(note.user.toString() !== req.user.id){           //User is trying to access notes of other user    
+        return res.status(401).send("Not Allowed");
+       }
+       note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+       res.json({note});
+    }catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
+})
+
+
+// ROUTE 4: Delete an existing Note using : DELETE "api/auth/deletenote". Login required
+router.delete("/deletenote/:id", fetchuser, async (req,res)=>{
+  const {title, description, tag} = req.body;
+  try {
+     //Find the note to be deleted amd delete it
+     let note = await Note.findById(req.params.id);
+     if(!note){return res.status(404).send("Not Found")} //Note is not there in the database
+     //Allow deletion only if user owns this Note
+     if(note.user.toString() !== req.user.id){      
+      return res.status(401).send("Not Allowed");
+     }
+     note = await Note.findByIdAndDelete(req.params.id)
+     res.json({"Success":"Note has been deleted", note:note});
+  }catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+  }
+})
+
 module.exports = router;
+ 
